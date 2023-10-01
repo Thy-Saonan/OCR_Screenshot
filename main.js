d9da07934884235171b1282955573224b74f4385
Thy-Saonan <thysaonan007@gmail.com>
2023-10-01 23:28:11 +0700

    rearrange ui for better user experience

--- main.js
+++ main.js
@@ -11,25 +11,50 @@ window.addEventListener('DOMContentLoaded', function () {
     var image = document.querySelector('#image');
     var cropper = new Cropper(image, {
       autoCrop: false,
-      toggleDragModeOnDblclick: true,
+      toggleDragModeOnDblclick: false,
+      moveable: false,
+      viewMode: 3,
+      cropstart: function (e) {
+        console.log("START", e)
+      },
+      cropmove: function (e) {
+        document.getElementById("overlay").setAttribute("hidden", "");
+        cropBox = document.getElementsByClassName("cropper-crop-box")[0];
+        cropBoxDim = cropBox.getBoundingClientRect();
+        button = document.getElementById("convertText");
+        button.removeAttribute("hidden")
+        button.style = `top: ${cropBoxDim.top - button.clientHeight<0?0:cropBoxDim.top - button.clientHeight}px;left:${cropBoxDim.left}px;`
+      }
     });
+    document.getElementById("close").onclick = () => {
+      cropper.clear();
+      document.getElementById("overlay").removeAttribute("hidden");
+      document.getElementById("convertText").setAttribute("hidden", "")
+    }
+    window.onresize = function (event) {
+      cropper.reset();
+    };
+
     var button = this.document.querySelector("#getText");
     button.onclick = function () {
       var crop_image = cropper.getCroppedCanvas().toDataURL('image/jpeg');
-      cropper.replace(crop_image);
+      document.getElementById("loading").removeAttribute("hidden");
+      document.getElementById("getText").setAttribute("hidden", "")
       chrome.runtime.sendMessage({ type: "getText", imageData: crop_image });
       // Send image to sandbox to process
       iframe.contentWindow.postMessage(crop_image, "*");
     };
   });
 });
-
-window.addEventListener('message', function(event) {
+window.addEventListener('message', function (event) {
   item = JSON.parse(localStorage.getItem("OCR_TEXT"));
-  if(item==null){
+  if (item == null) {
     item = []
   }
   const withoutLineBreaks = event.data.replace(/[\r\n]/gm, '');
-  item.push(withoutLineBreaks);
+  item.unshift(withoutLineBreaks);
   localStorage.setItem("OCR_TEXT", JSON.stringify(item));
+  document.getElementById("getText").removeAttribute("hidden");
+  document.getElementById("loading").setAttribute("hidden", "");
+  alert(event.data);
 });
