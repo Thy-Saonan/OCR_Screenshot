31554392c713280710b424ff3970c9498e930bf0
Thy-Saonan <thysaonan007@gmail.com>
2023-10-01 14:03:31 +0700

    Add search feature in clipboard

--- popup.js
+++ popup.js
@@ -1,26 +1,51 @@
 ﻿var screenshotImage = "";
 let notes = JSON.parse(localStorage.getItem("OCR_TEXT"))
-if(notes==null){
-  notes = []
-}
-let listGroup = document.querySelector("#list-group")
-notes.forEach(note => {
-  let a = document.createElement("a");
-  a.setAttribute("href", "#");
-  a.setAttribute("class", "list-group-item list-group-item-action");
-  a.setAttribute("data-bs-toggle", "tooltip");
-  a.setAttribute("data-bs-placement", "bottom");
-  a.setAttribute("title", "Click to copy");
-  a.onclick = ()=>{
-    navigator.clipboard.writeText(note);
+function renderList(notes){
+  if(notes==null){
+    notes = []
   }
-  a.innerText = note
-  listGroup.appendChild(a);
-});
-document.getElementById("take_screenshot").onclick = async () => {
-  const screenshot = await chrome.tabs.captureVisibleTab();
-  screenshotImage = screenshot;
-  chrome.runtime.sendMessage({ type: "screenshot", imageData: screenshot });
-  iframe.contentWindow.postMessage(screenshot, "*");
+  let listGroup = document.querySelector("#list-group")
+  listGroup.innerHTML = "";
+  let header = document.createElement("a");
+  header.setAttribute("href", "#");
+  header.setAttribute("class","list-group-item list-group-item-action active");
+  header.setAttribute("aria-current", "true");
+  header.innerText = "Text From OCR";
+  listGroup.appendChild(header)
+  notes.forEach(note => {
+    let a = document.createElement("a");
+    a.setAttribute("href", "#");
+    a.setAttribute("class", "list-group-item list-group-item-action");
+    a.setAttribute("data-bs-toggle", "tooltip");
+    a.setAttribute("data-bs-placement", "bottom");
+    a.setAttribute("title", "Click to copy");
+    a.onclick = ()=>{
+      navigator.clipboard.writeText(note);
+    }
+    a.innerText = note
+    listGroup.appendChild(a);
+  });
 }
+renderList(notes)
+function search(searchTerm){
+  let notes = JSON.parse(localStorage.getItem("OCR_TEXT"))
+  if(notes==null){
+    notes = []
+  }
+  newitem = notes.filter(function(item){return item.toLowerCase().includes(searchTerm.toLowerCase());})
+  renderList(newitem)
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  document.getElementById("searchInput").addEventListener("input", function(input){
+    console.log(input.target.value)
+    search(input.target.value)
+  })
+  document.getElementById("take_screenshot").onclick = async () => {
+    const screenshot = await chrome.tabs.captureVisibleTab();
+    screenshotImage = screenshot;
+    chrome.runtime.sendMessage({ type: "screenshot", imageData: screenshot });
+    iframe.contentWindow.postMessage(screenshot, "*");
+  }
+});
 
